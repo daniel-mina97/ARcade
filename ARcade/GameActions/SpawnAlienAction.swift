03421353c34ace236b58f6f53da2894d6ed08075
Daniel Mina <minadaniel27@gmail.com>
2018-11-08 09:22:37 -0600

    Add GameAction classes to project
    Created several empty classes for future implementation.

---
+++ ARcade/GameActions/SpawnAlienAction.swift
@@ -0,0 +1,11 @@
+//
+//  SpawnAction.swift
+//  ARcade
+//
+//  Created by Daniel Mina on 11/8/18.
+//  Copyright © 2018 University of Houston-Clear lake (ARGuys). All rights reserved.
+//
+
+import Foundation
+
+class SpawnAlienAction : PGameAction {}
