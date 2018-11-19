308fba73b184c4717f22cb237f0b1c97c109f234
Daniel Mina <minadaniel27@gmail.com>
2018-11-18 18:45:07 -0600

    Expanded GameAction class
    The GameAction class has been built up as one class that will have a 'type' field used in a switch statement in the execute function of the game manager.

---
+++ ARcade/Game/GameActionHandling/GameAction.swift
@@ -0,0 +1,28 @@
+//
+//  PGameAction.swift
+//  ARcade
+//
+//  Created by Daniel Mina on 11/8/18.
+//  Copyright © 2018 University of Houston-Clear lake (ARGuys). All rights reserved.
+//
+
+import Foundation
+
+class GameAction {
+    
+    static var overallActionID: Int = 0
+    
+    let type: ActionTypes
+    let actionID: Int
+    let sourceID: Int?
+    let targetID: Int?
+    
+    init(type: ActionTypes) {
+        self.type = type
+        self.actionID = GameAction.overallActionID
+        GameAction.overallActionID += 1
+        self.sourceID = nil
+        self.targetID = nil
+    }
+}
+
