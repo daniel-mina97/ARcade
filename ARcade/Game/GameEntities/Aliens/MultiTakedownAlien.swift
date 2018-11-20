69a07e9f0947fbbd0b99540b3e776469345d6fc2
Nathan John Dill <25229416+dojonate@users.noreply.github.com>
2018-11-20 09:20:31 -0600

    Moved MultiTakedownAlien class

--- ARcade/Game/GameEntities/Aliens/MultiTakedownAlien.swift
+++ ARcade/Game/GameEntities/Aliens/MultiTakedownAlien.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+class MultiTakedownAlien: Alien {
+    //
+    //    var maxHealth: Int // int: maximum health of vessel
+    //    var health: Int // int: remaining health of vessel
+    //    var speed: Int // int: speed of vessel
+    //    var damage: Int //Invasion Damage: amount of damage to deal to City upon arrival
+    //    var difficulty: Int // Variable that changes depending on room creator's difficulty setting
+    //    var model:SCNNode? //Model/Sprite: points to physical model
+    
+    init(){
+        maxHealth = 4
+        
+    }
+    func TakeDamage(<#parameters#>) -> <#return type#> {
+        <#function body#>
+    }
+    
+}
+
