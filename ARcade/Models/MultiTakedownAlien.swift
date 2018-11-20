2f8d477be410af517f1d04f295670730028b8b3a
Nathan John Dill <25229416+dojonate@users.noreply.github.com>
2018-11-20 09:05:17 -0600

    Added MultiTakedownClass

---
+++ ARcade/Models/MultiTakedownAlien.swift
@@ -0,0 +1,13 @@
+//
+//  MultiTakedownAlien.swift
+//  ARcade
+//
+//  Created by Nathan Dill on 11/7/18.
+//  Copyright © 2018 University of Houston-Clear lake (ARGuys). All rights reserved.
+//
+
+import Foundation
+class MultiTakedownAlien: Alien {
+    
+    
+}
